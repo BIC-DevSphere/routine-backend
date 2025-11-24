0a241990d407732687aaf01db7db9afdd4da8b45
adgator101 <aadityathapa977@gmail.com>
2025-11-24 23:15:20 +0545

    feat: enhance service modules with createOrGet functionality for modules, rooms, and teachers

--- src/services/module.service.ts
+++ src/services/module.service.ts
@@ -1,11 +1,30 @@
 import type { Module, PrismaClient } from "@prisma/client";
 import { type BaseService, createBaseService } from "./base.service";
 
-export type ModuleService = BaseService<Module>;
+export type ModuleService = BaseService<Module> & {
+	createOrGetModule(
+		data: { code: string; name: string },
+		tx: PrismaClient,
+	): Promise<Module>;
+};
 export function createModuleService(prisma: PrismaClient): ModuleService {
 	const baseService = createBaseService<Module>(prisma.module);
 
 	return {
 		...baseService,
+		async createOrGetModule(
+			data: { code: string; name: string },
+			tx: PrismaClient,
+		) {
+			const moduleResult = await tx.module.upsert({
+				where: { moduleCode: data.code },
+				update: {},
+				create: {
+					name: data.name || "Unnamed Module",
+					moduleCode: data.code || "NO_CODE",
+				},
+			});
+			return moduleResult;
+		},
 	};
 }
